6006b06c9ed848938cb7831c17d6e0563ffc1eee
Akash <akashpeter070@gmail.com>
2025-12-31 12:27:42 +0530

    Add migration files and fix build script

--- build.sh
+++ build.sh
@@ -3,10 +3,10 @@
 set -o errexit
 
 # Install dependencies
-pip install Pillow
 pip install -r requirements.txt
 
-# Run migrations
-python manage.py makemigrations myapp
+# Run migrations (Render should only EXECUTE migrations, not create them)
 python manage.py migrate
-python manage.py createsuperuser --no-input || true
+
+# Create superuser if needed
+# python manage.py createsuperuser --no-input || true
